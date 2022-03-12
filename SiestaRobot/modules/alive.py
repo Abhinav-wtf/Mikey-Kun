3b3a3e1740f5863388015bcd2d0045fa33f47959
XDHate#Ded <98488953+XhateXd@users.noreply.github.com>
2022-03-12 10:26:24 +0530

    Update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -9,16 +9,16 @@ from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/ff2fa22dfa6ae838cc6cd.jpg"
+PHOTO = "https://telegra.ph/file/92487bd1cc9fdba41c32e.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Siesta Robot.** \n\n"
-  TEXT += "💠 **I'm Working Properly** \n\n"
-  TEXT += f"💠 **My Master : [Vain](https://t.me/saint_foire)** \n\n"
-  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
-  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "🔹 **I'm Working Properly** \n\n"
+  TEXT += f"🔹 **My Master : [Mikey-kun](https://t.me/mikey_kun90)** \n\n"
+  TEXT += f"🔹 **Library Version :** `{telever}` \n\n"
+  TEXT += f"🔹 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"🔹 **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Siestaxbot?start=help"), Button.url("Support", "https://t.me/machinaxsupport")]]
+  BUTTON = [[Button.url("Help", "https://t.me/mikeyxrobot?start=help"), Button.url("Support", "https://t.me/MikeyXSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
