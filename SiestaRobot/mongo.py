b11030c7bcabae8b42d531a4ba0117c85fe5f1cb
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-05-28 06:40:45 +0530

    Update mongo.py

--- SiestaRobot/mongo.py
+++ SiestaRobot/mongo.py
@@ -3,20 +3,14 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from SiestaRobot import MONGO_DB_URI
 from SiestaRobot.confing import get_int_key, get_str_key
 from SiestaRobot import LOGGER
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "SiestaRobot"
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB]
-db = client["VainBot"]
+client = MongoClient("mongodb+srv://mike:mike123@cluster0.gl7x8.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://mike:mike123@cluster0.gl7x8.mongodb.net/?retryWrites=true&w=majority", 27017)["SiestaRobot"]
+motor = motor_asyncio.AsyncIOMotorClient("mongodb+srv://mike:mike123@cluster0.gl7x8.mongodb.net/?retryWrites=true&w=majority", 27017)
+db = motor["SiestaRobot"]
+db = client["SiestaRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
