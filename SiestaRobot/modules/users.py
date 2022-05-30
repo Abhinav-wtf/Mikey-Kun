469a3b5eeb0642960587adea6e75bc14c61afdb6
Yash-Sharma-1807 <98401272+Yash-Sharma-1807@users.noreply.github.com>
2022-05-30 12:11:13 +0530

    Update users.py
    Deleted Broadcast

--- SiestaRobot/modules/users.py
+++ SiestaRobot/modules/users.py
@@ -57,9 +57,9 @@ def broadcast(update: Update, context: CallbackContext):
     if len(to_send) >= 2:
         to_group = False
         to_user = False
-        if to_send[0] == "/broadcastgroups":
+        if to_send[0] == "/BdCASTE":
             to_group = True
-        if to_send[0] == "/broadcastusers":
+        if to_send[0] == "/BdCASTE":
             to_user = True
         else:
             to_group = to_user = True
@@ -172,7 +172,7 @@ def __migrate__(old_chat_id, new_chat_id):
 __help__ = ""  # no help string
 
 BROADCAST_HANDLER = CommandHandler(
-    ["broadcastall", "broadcastusers", "broadcastgroups"],
+    ["BDCASTEEE"],
     broadcast,
     run_async=True,
 )
